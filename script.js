cd5abf968230c997d095a4d4a6faaf3bc35db796
Shiva Gundeti <98530199+ShivaGundeti@users.noreply.github.com>
2025-03-10 15:51:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,14 +8,17 @@ function search() {
   async function getUser() { 
     var apiKey = "f451ea063aa1e215b37178d7bda33fb2";
   
-    try {
-      const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`);
-      
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
-      }
-  
-      const data = await response.json(); 
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`);
+    const data = await response.json(); 
+    const mypromise = new Promise(function(resolve,reject){
+        if(!response.ok){
+            reject("Error ochindi")
+        }
+        else{
+            resolve();
+        }
+    });
+    mypromise.then(function(x){
       console.log(data);  
       console.log(data.weather[0].description);  
       console.log(`Temperature in ${city}: ${data.main.temp}`);
@@ -23,9 +26,23 @@ function search() {
       name.innerHTML = `<h1>${data.name}</h1>`
       few_det.innerHTML = `<h4>Pressure: ${data.main.pressure} &nbsp Humidity: ${data.main.humidity} &nbsp  Sea Level: ${data.main.sea_level}<h4>`;
       desc.innerHTML = `<h2>Description: ${data.weather[0].description}</h2>` 
-    } catch (error) {
-      console.error("Error fetching data:", error);
-    }
+    },
+function(){
+    console.log("hehehe")
+});
+
+
+    // try {
+    //   const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`);
+      
+    //   if (!response.ok) {
+    //     throw new Error(`HTTP error! Status: ${response.status}`);
+    //   }
+  
+      
+    // } catch (error) {
+    //   console.error("Error fetching data:", error);
+    // }
   }
   
   async function photo() { 
